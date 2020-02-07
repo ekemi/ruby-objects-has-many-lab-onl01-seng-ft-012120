94dc4118bb1933ab185bfa30c9fac3b3017339d3
poignant-writer-1163 <edmundopierre@yahoo.com>
2020-02-07 15:44:55 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,11 +1,18 @@
+<<<<<<< HEAD
 class Author 
   
   attr_accessor :name
   @@all=[]
+=======
+def Author 
+  
+  attr_accessor :name,:post
+>>>>>>> a851e079678a7409b0700c2b2c498bf85622d367
   
   def initialize (name)
     
     @name = name
+<<<<<<< HEAD
      @@all << self
   end
   
@@ -33,6 +40,16 @@ class Author
 
   def add_post (post)
     post.author =self
+=======
+    
+  end
+  
+  def add_post(post)
+    
+    
+    
+    
+>>>>>>> a851e079678a7409b0700c2b2c498bf85622d367
   end
   
 end
